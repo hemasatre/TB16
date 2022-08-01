3a6ee3149a3a15499648c6136f33676df3f110ac
hemasatre <110393176+hemasatre@users.noreply.github.com>
2022-08-01 19:02:24 +0530

    Update Demo.java
    By Git

--- GitHub/src/com/cjc/Demo.java
+++ GitHub/src/com/cjc/Demo.java
@@ -7,6 +7,11 @@ public class Demo
 	{
 		System.out.println("m1");
 	}
+	
+	public void m2()
+	{
+		System.out.println("m2");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Git");
